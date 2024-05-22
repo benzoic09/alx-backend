5c72641bcd647d1060e6d807d0ff8704b31d4a8d
Ben Kimani <benzoic09@gmail.com>
2024-05-22 18:19:17 +0000

    caching task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""MRUCache module"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRUCache class that inherits from BaseCaching and is an
+    MRU caching system"""
+
+    def __init__(self):
+        """Initialize the class"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            # Move the key to the end to mark it as recently used
+            self.cache_data.move_to_end(key)
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # Pop the last item (most recently used)
+            most_recent_key, _ = self.cache_data.popitem(last=True)
+            print(f"DISCARD: {most_recent_key}")
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        # Move the key to the end to mark it as recently used
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]

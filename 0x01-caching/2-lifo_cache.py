b9eeafc5a36d71f7417ef2f8eac9ecf9ec0ec058
Ben Kimani <benzoic09@gmail.com>
2024-05-22 17:19:56 +0000

    caching task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
-"""task 2"""
+"""Task 2: LIFO Caching.
+"""
 from collections import OrderedDict
+
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """LIFOCache class that inherits from BaseCaching and is a
-    LIFO caching system"""
-
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a LIFO
+    removal mechanism when the limit is reached.
+    """
     def __init__(self):
-        """Initialize the class"""
+        """Initializes the cache.
+        """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Add an item in the cache"""
+        """Adds an item in the cache.
+        """
         if key is None or item is None:
             return
         if key not in self.cache_data:
@@ -25,7 +30,6 @@ class LIFOCache(BaseCaching):
         self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
-        """Get an item by key"""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)

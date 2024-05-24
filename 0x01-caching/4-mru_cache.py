47aa8697d49c2ce98159727644fa9b0405ee12d1
Ben Kimani <benzoic09@gmail.com>
2024-05-24 09:57:35 +0000

    cache task 4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -13,7 +13,6 @@ class MRUCache(BaseCaching):
         super().__init__()
         self.cache_data = OrderedDict()
         self.MAX_ITEMS = BaseCaching.MAX_ITEMS
-        self.cache_data_updated = {}
 
     def put(self, key, item):
         """Add an item in the cache"""
@@ -33,7 +32,6 @@ class MRUCache(BaseCaching):
 
     def get(self, key):
         """Get an item by key"""
-        if key in self.cache_data_updated:
-            self.cache_data_updated[key] = self.cache_data_updated.pop(key)
-            return self.cache_data_updated[key]
-        return None
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]

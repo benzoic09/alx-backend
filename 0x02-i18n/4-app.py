fff61afbc94aef03236f52674801f31bd6bc311d
Ben Kimani <benzoic09@gmail.com>
2024-05-28 21:14:01 +0000

    task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""A Basic Flask app with Babel integration"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """Configuration for Flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match for supported languages."""
+    # Check if the locale parameter is in the request and if it is valid
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    # Fallback to the best match for supported languages
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Basic HTML template"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()

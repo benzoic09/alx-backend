110c54114a0eae438e75a56e77ef35bef34f946c
Ben Kimani <benzoic09@gmail.com>
2024-05-28 18:29:16 +0000

    basic flask

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" basic flask"""
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    """default route"""
+    return render_templete("0-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
